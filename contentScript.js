e29d0571cdd893cf0af9b6e27c57612fd692ef88
James Smith(杨鑫) <xin.y@trip.com>
2024-04-03 17:26:04 +0800

    fix: 修复用户自定义颜色后，刷新页面后失效的问题

--- contentScript.js
+++ contentScript.js
@@ -13,3 +13,19 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         document.head.appendChild(styleElement);
     }
 });
+
+
+chrome.storage.sync.get().then((items) => {
+    var backgroundColor = items.backgroundColor;
+    var fontColor = items.fontColor;
+
+    var styleElement = document.createElement('style');
+    styleElement.innerHTML = `
+      ::selection {
+        color: ${fontColor} !important;
+        background-color: ${backgroundColor} !important;
+      }
+    `;
+    document.head.appendChild(styleElement);
+});
+
